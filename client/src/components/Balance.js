36c2a4d358c6a04663d63102eb06425b31bb5372
Deepak Pun <deepakpun@Deepaks-MacBook-Pro.local>
2024-08-17 14:41:26 -0400

    Frontend and Backend complete. Ready for deployment

--- client/src/components/Balance.js
+++ client/src/components/Balance.js
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
+import { numberWithCommas } from '../utils/format'
 
 export const Balance = () => {
 	const { transactions } = useContext(GlobalContext)
 	// console.log(transactions)
 
 	const amounts = transactions.map(transaction => transaction.amount)
-	const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+	const total = numberWithCommas(
+		amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+	)
 
 	return (
 		<>
